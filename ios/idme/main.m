5b645c471de20ab30a0f967f070d25d849718d26
jrburke <jrburke@gmail.com>
2011-11-29 16:08:59 -0800

    Baseline ios project, no browserid stuff yet.

---
+++ ios/idme/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  idme
+//
+//  Created by James Burke on 11/29/11.
+//  Copyright Canada 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
